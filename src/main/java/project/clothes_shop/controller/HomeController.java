39fdc21b3bcddb8848750a774fa88498250b2b7b
Phan Thế Kỷ <thekypccc@gmail.com>
2021-04-05 00:19:40 +0700

    handle login error, add fragment for layout, update header

--- src/main/java/project/clothes_shop/controller/HomeController.java
+++ src/main/java/project/clothes_shop/controller/HomeController.java
@@ -1,13 +1,25 @@
 package project.clothes_shop.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+import project.clothes_shop.model.AppUser;
+import project.clothes_shop.service.user.IAppUserService;
 
 @RestController
 @RequestMapping("/")
 public class HomeController {
+    @Autowired
+    private IAppUserService appUserService;
+
+    @ModelAttribute("user")
+    public AppUser getCurrentUser() {
+        return appUserService.getCurrentUser();
+    }
+
     @GetMapping("")
     public ModelAndView showIndex() {
         return new ModelAndView("user/index");
--- src/main/java/project/clothes_shop/controller/LoginController.java
+++ src/main/java/project/clothes_shop/controller/LoginController.java
@@ -3,12 +3,13 @@ package project.clothes_shop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/")
 public class LoginController {
-    @GetMapping("")
+    @GetMapping("/login")
     public ModelAndView showIndex() {
         return new ModelAndView("user/login");
     }
