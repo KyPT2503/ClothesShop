408b6c89bddae8ae36c6b9dce9f100a41ba9bbca
Phan Thế Kỷ <thekypccc@gmail.com>
2021-04-15 13:09:02 +0700

    update index

--- src/main/java/project/clothes_shop/controller/HomeController.java
+++ src/main/java/project/clothes_shop/controller/HomeController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
-import project.clothes_shop.model.AppUser;
-import project.clothes_shop.model.Cart;
+import project.clothes_shop.model.*;
+import project.clothes_shop.service.brand.IBrandService;
 import project.clothes_shop.service.cart.ICartService;
+import project.clothes_shop.service.category.ICategoryService;
+import project.clothes_shop.service.clothes.IClothesService;
 import project.clothes_shop.service.user.IAppUserService;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -20,6 +23,33 @@ public class HomeController {
     private IAppUserService appUserService;
     @Autowired
     private ICartService cartService;
+    @Autowired
+    private IClothesService clothesService;
+    @Autowired
+    private ICategoryService categoryService;
+    @Autowired
+    private IBrandService brandService;
+
+    @ModelAttribute("brands")
+    public List<Brand> getAllBrand() {
+        return brandService.findAll();
+    }
+
+    @ModelAttribute("categories")
+    public List<Category> getAllCategory() {
+        return categoryService.findAll();
+    }
+
+    @ModelAttribute("muchSold")
+    public List<Clothes> getTop5SoldAmount() {
+        return clothesService.findTop5BySoldAmount(0);
+    }
+
+    @ModelAttribute("muchView")
+    public List<Clothes> getTop5ViewCount() {
+        return clothesService.findTop5ByViewCount(0);
+    }
+
     @ModelAttribute("current_cart")
     public Cart getCurrentCart(HttpSession session) {
         return cartService.getCurrentCart(session);
